36d9a73e5c732d5d9e018d8ff6e1699399ca687c
Swagnick99 <sagnikbasu19@gmail.com>
2020-06-09 21:51:33 +0530

    Identifier added

---
+++ identifier.py
@@ -0,0 +1,37 @@
+import keyword
+import string
+
+
+class ID(keyword.Matcher):
+    qf = 1
+    alpha_list = list(string.ascii_lowercase + string.ascii_uppercase + "_")  # alphabet list + "_"
+    alnum_list = list(string.digits + string.ascii_lowercase + string.ascii_uppercase + "_")  # alphabet list +
+    # number list + "_"
+
+    stateTable = [
+        {x: 1 for x in alpha_list},  # q0
+        {y: 1 for y in alnum_list}  # q1
+    ]
+
+    def match(self, currentState, char):
+        return super().nextState(currentState, char, self.stateTable)
+
+
+def IDMatch(word):
+    matchObj = ID()
+    state = 0
+
+    for ch in word:
+        state = matchObj.match(state, ch)  # go to new state
+    if state == matchObj.qf:
+        return True  # valid ID
+    else:
+        return False
+
+
+word = input()
+
+if IDMatch(word) and not keyword.KeywordMatcher().keyMatch(word):
+    print("Valid identifier")
+else:
+    print("Not a valid one")
